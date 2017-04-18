63d60820b101a1b8ea9b796189d866cd70eb3bfc
Vicky Ledsom <victorialedsom@gmail.com>
2017-04-18 20:55:16 +0100

    Add first test for printing statement - that it includes the header

--- spec/StatementSpec.js
+++ spec/StatementSpec.js
@@ -6,4 +6,14 @@ var statement;
   beforeEach(function() {
     statement = new Statement();
   });
+
+  // it("is instantiated with the Statement class by default", function() {
+  //   expect(statement.transactionHistory).toEqual(jasmine.any(TransactionHistory));
+  // });
+
+  describe("print", function() {
+    it("Prints the statement", function() {
+      expect(statement.print()).toEqual("date || credit || debit || balance");
+    });
+  });
 });
--- src/Statement.js
+++ src/Statement.js
@@ -3,7 +3,14 @@
 (function(exports) {
 
   function Statement() {
+    // this.transactionHistory = transactionHistory;
   }
-  
+
+  var header = "date || credit || debit || balance";
+
+  Statement.prototype.print = function () {
+    return header;
+  };
+
   exports.Statement = Statement;
 })(this);
